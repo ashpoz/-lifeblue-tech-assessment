960435b4506393bf2e0848351eec099d55a1206c
hellomaryjo <majorx3music@gmail.com>
2023-06-08 01:45:33 -0500

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server"
+  output: "server",
+  adapter: netlify()
 });
